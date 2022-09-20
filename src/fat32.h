34a61fd5856163f5cfcad46511df5750f048cdd8
fparadas <felipegparadas@gmail.com>
2022-09-20 20:17:51 -0300

    defined a structure to follow

---
+++ src/fat32.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// funções disponibilizadas para mexer em arquivos
+int ffopen();
+int ffclose();
+int ffseek();
+int ffread();
+int ffwrite();
---
+++ src/ui.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// funções de integração com o usuário
+int ls();
+int cd();
+int touch();
+int mkdir();
+int rm();
+int rmdir();
